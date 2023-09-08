d48eeec690b09f9078931bb3c841d541d4026ff6
Abubakar <amoo.akin90@gmail.com>
2023-09-08 14:36:46 -0700

    lets put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - first case of c
+ *
+ * RETURN: always end
+ */
+
+int main (void)
+{
+     puts("\"Programming is like building a multilingual puzzle);
+     return (o);
+}
